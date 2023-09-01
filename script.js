5a8d0b971630f2f418d35d188d13bda4ca269863
subhaishere <60627114+subhaishere@users.noreply.github.com>
2023-09-01 18:36:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
 //your JS code here. If required.
+
+let prom1 = new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		let t1=performance.now();
+		resolve(t1);
+	},1000)
+})
+let prom2 = new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		let t2=performance.now();
+		resolve(t2);
+	},1500)
+})
+
+let prom3 = new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		let t3=performance.now();
+		resolve(t3);
+	},2000)
+})
+
+let prom = Promise.all([prom1,prom2,prom3]);
+prom.then((arr)=>{
+	let tbody = document.getElementById("output");
+	let sum=0;
+	tbody.children[0].remove();
+	for (let i = 0; i <=arr.length; i++) {
+		
+		let newRow = document.createElement("tr");
+		let col1= document.createElement("td");
+		let col2= document.createElement("td");
+		
+		if(i==3){
+			col1.innerText=`Total`
+		    col2.innerText=(sum/1000).toFixed(3);
+		}
+		else{
+			sum+=arr[i];
+			col1.innerText=`Promise${i+1}`
+		    col2.innerText=(arr[i]/1000).toFixed(3);
+		}
+		
+		
+		newRow.appendChild(col1);
+		newRow.appendChild(col2);
+		tbody.appendChild(newRow);
+	}
+})
